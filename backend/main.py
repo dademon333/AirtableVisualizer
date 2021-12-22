58af37ec382716212fbe3658ee6cef2478a00d6c
dademon333 <dademon333@mail.ru>
2021-12-22 23:56:36 +0500

    Add CORS

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.handlers import api_router
 
 app = FastAPI()
 app.include_router(api_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_origin_regex=r'https?://localhost:[0-9]{1,5}',
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
