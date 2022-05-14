9e95e9cd89f9248152bae59d3d64727e956b333c
dademon333 <dademon333@mail.ru>
2022-05-15 01:33:02 +0500

    Update api routes

--- backend/root_router.py
+++ backend/root_router.py
@@ -13,36 +13,36 @@ root_router = APIRouter()
 root_router.include_router(stuff_router)
 root_router.include_router(
     users_router,
-    prefix='/users',
+    prefix='/api/users',
     tags=['Users']
 )
 
 root_router.include_router(
     courses_router,
-    prefix='/courses',
+    prefix='/api/courses',
     tags=['Courses']
 )
 
 root_router.include_router(
     entities_router,
-    prefix='/entities',
+    prefix='/api/entities',
     tags=['Entities']
 )
 
 root_router.include_router(
     entities_connections_router,
-    prefix='/entities_connections',
+    prefix='/api/entities_connections',
     tags=['Entities connections']
 )
 
 root_router.include_router(
     types_connections_router,
-    prefix='/types_connections',
+    prefix='/api/types_connections',
     tags=['Types connections']
 )
 
 root_router.include_router(
     change_log_router,
-    prefix='/change_log',
+    prefix='/api/change_log',
     tags=['Change Log']
 )
