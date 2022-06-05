891b53f14248e3ca68b80021355c96c1311e455b
thefirst24 <sharipow777@gmail.com>
2022-06-05 22:45:49 +0500

    features

--- graph/src/index.tsx
+++ graph/src/index.tsx
@@ -8,11 +8,23 @@ import store from './libs/redux/store';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render(
-  <Provider store={store}>
-      <App />
-  </Provider>
-);
+
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+  root.render(
+      <Provider store={store}>
+          <App />
+      </Provider>
+  );
+} else {
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+          <App />
+      </Provider>
+    </React.StrictMode>
+  );
+}
+
 
 //<React.StrictMode>
 //</React.StrictMode>
