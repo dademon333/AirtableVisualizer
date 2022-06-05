a947ceaeb2687a08886ecbc81fb026d29e2f14c6
dademon333 <dademon333@mail.ru>
2022-06-06 01:10:17 +0500

    Update frontend building

--- backend/middlewares/html_page.py
+++ backend/middlewares/html_page.py
@@ -3,7 +3,8 @@ from fastapi.templating import Jinja2Templates
 
 from config import Config
 
-templates = Jinja2Templates(directory=Config.FRONT_ROOT)
+graph_templates = Jinja2Templates(directory=Config.GRAPH_FRONT_ROOT)
+table_templates = Jinja2Templates(directory=Config.TABLE_FRONT_ROOT)
 
 
 async def html_page_middleware(request: Request, call_next):
@@ -15,6 +16,13 @@ async def html_page_middleware(request: Request, call_next):
     if request.method == 'GET' \
             and 'text/html' in request.headers.get('accept', '')\
             and request.url.path not in [request.app.docs_url, request.app.redoc_url]:
-        return templates.TemplateResponse('index.html', {'request': request})
+        if request.url.path.startswith('/table'):
+            return table_templates.TemplateResponse(
+                'index.html', {'request': request}
+            )
+        else:
+            return graph_templates.TemplateResponse(
+                'index.html', {'request': request}
+            )
 
     return await call_next(request)
